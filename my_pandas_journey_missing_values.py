6d2d84f6780ba23a9033f2590c8436abfd9aa6c9
Judicaeld17e <judicaeldjidonou3@gmail.com>
2023-11-26 10:16:41 +0100

    first commit

---
+++ my_pandas_journey_missing_values.py
@@ -0,0 +1,4 @@
+# TASK: Create a function which receive a pandas dataframe and returns a datafrom transformated by isnull().
+import pandas as pd
+def my_pandas_journey_missing_values(df):
+    return df.isnull()
